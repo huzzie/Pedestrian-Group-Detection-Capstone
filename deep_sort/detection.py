cd5ad1e0eb1561abd5bf36b239b93f5527d2dc3b
Hyunjae Cho <49483133+huzzie@users.noreply.github.com>
2021-04-01 17:18:01 +0900

    Add files via upload

---
+++ deep_sort/detection.py
@@ -0,0 +1,51 @@
+
+#%%
+import sys
+import numpy as np
+
+
+class Detection(object):
+    """
+    This class represents a bounding box detection in a single image.
+    Parameters
+    ----------
+    tlwh : array_like
+        Bounding box in format `(x, y, w, h)`.
+    confidence : float
+        Detector confidence score.
+    feature : array_like
+        A feature vector that describes the object contained in this image.
+    Attributes
+    ----------
+    tlrb : ndarray
+        Bounding box in format `(top x, left y, right, bottom)`.
+    confidence : ndarray
+        Detector confidence score.
+    feature : ndarray | NoneType
+        A feature vector that describes the object contained in this image.
+    """
+
+    def __init__(self, tlrb, confidence, feature):
+        self.tlbr = np.asarray([tlrb[0], tlrb[1], tlrb[3], tlrb[2]], dtype=np.float32)
+        self.tlwh = np.asarray([tlrb[0], tlrb[1], tlrb[2]-tlrb[0], tlrb[3]-tlrb[1]], dtype=np.float32)
+        self.confidence = float(confidence)
+        self.feature = np.asarray(feature, dtype=np.float32)
+
+    def to_tlbr(self):
+        """Convert bounding box to format `(min x, min y, max x, max y)`, i.e.,
+        `(top left, bottom right)`.
+        """
+        return self.tlbr
+
+    def to_xyah(self):
+        """Convert bounding box to format `(center x, center y, aspect ratio,
+        height)`, where the aspect ratio is `width / height`.
+        """
+        ret = self.tlwh.copy()
+        ret[:2] += ret[2:] / 2
+        ret[2] /= ret[3]
+        return ret
+
+
+#%%
+print('hello')
